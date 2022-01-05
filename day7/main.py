18f686bc06b915842c818d1341a98fd38752dfca
CedricDoerr <doerr.cedric@gmail.com>
2022-01-05 16:02:02 +0100

    Day 7 finished

--- day7/main.py
+++ day7/main.py
@@ -1,10 +1,31 @@
+from os import confstr
 import statistics as sts
 with open('/Users/cedricdorr/Documents/Programmieren/AdventOfCode/day7/input.txt') as f:
     data = [int(x) for x in f.read().split(',')]
-median = sts.median(data)
-fuel = 0
+mean = round(sts.mean(data))
 
-for crab in data:
-    fuel += abs(crab - median)
+def findFuel(dist):
+    cost = 0
+    for i in range(0,dist+1):
+        cost += i
+    return cost
 
-print(fuel)
+def algorithm(lvl, crabs):
+    fuel = 0
+    for crab in crabs:
+        fuel += findFuel(abs(crab-lvl))
+    return fuel
+
+fuelcost = 0
+guess = mean
+
+while True:
+    if(algorithm(guess-1, data) < algorithm(guess, data)):
+        guess -= 1
+    if(algorithm(guess+1, data) < algorithm(guess, data)):
+        guess += 1
+    else:
+        fuelcost = algorithm(guess, data)
+        break
+
+print(fuelcost)
